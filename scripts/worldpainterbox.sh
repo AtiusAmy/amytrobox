386337229477cc9272fa002ad18c86a68bbb591e
Atius Amy <torv.device027@passmail.net>
2025-05-03 04:40:48 +0700

    Update worldpainter

--- scripts/worldpainterbox.sh
+++ scripts/worldpainterbox.sh
@@ -7,10 +7,12 @@
 
 apt update -y && apt upgrade -y
 apt install wget tar -y
-wget -P /tmp/ https://www.worldpainter.net/files/worldpainter_2.23.2.deb
+wget -P /tmp/ https://www.worldpainter.net/files/worldpainter_2.24.0.deb
 
-apt install /tmp/worldpainter_2.23.2.deb -y
-rm /tmp/worldpainter_2.23.2.deb
-wget -P /usr/share/themes https://github.com/lassekongo83/adw-gtk3/releases/download/v6.1/adw-gtk3v6.1.tar.xz
-tar -xvf /usr/share/themes/adw-gtk3v6.1.tar.xz
+apt install /tmp/worldpainter_2.24.0.deb -y
+rm /tmp/worldpainter_2.24.0.deb
+curl -s https://julianfairfax.codeberg.page/package-repo/pub.gpg | gpg --dearmor | dd of=/usr/share/keyrings/julians-package-repo.gpg
+
+echo 'deb [ signed-by=/usr/share/keyrings/julians-package-repo.gpg ] https://julianfairfax.codeberg.page/package-repo/debs packages main' | tee /etc/apt/sources.list.d/julians-package-repo.list
+apt update -y && apt install adw-gtk3 -y
 
