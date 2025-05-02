28c6d2045ba5b3422ca90540c8b0f8a77a7d1541
Atius Amy <torv.device027@passmail.net>
2025-05-03 04:19:18 +0700

    I forgot how apt works

--- scripts/worldpainterbox.sh
+++ scripts/worldpainterbox.sh
@@ -7,10 +7,10 @@
 
 apt update -y && apt upgrade -y
 apt install wget tar -y
-wget https://www.worldpainter.net/files/worldpainter_2.23.2.deb
+wget -P /tmp/ https://www.worldpainter.net/files/worldpainter_2.23.2.deb
 
-apt install worldpainter_2.23.2.deb -y
-rm worldpainter_2.23.2.deb
+apt install /tmp/worldpainter_2.23.2.deb -y
+rm /tmp/worldpainter_2.23.2.deb
 wget -P /usr/share/theme https://github.com/lassekongo83/adw-gtk3/releases/download/v6.1/adw-gtk3v6.1.tar.xz
 tar -xvf /usr/share/theme/adw-gtk3v6.1.tar.xz
 
