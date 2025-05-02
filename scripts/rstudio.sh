8429c7b9a63b3b8fea5bf97447773a073721dbf6
Atius Amy <torv.device027@passmail.net>
2025-05-02 17:38:04 +0700

    Attempt to add Rstudio

--- scripts/rstudio.sh
+++ scripts/rstudio.sh
@@ -14,3 +14,4 @@ dnf -y install wget
 wget https://download1.rstudio.org/electron/rhel9/x86_64/rstudio-2024.12.1-563-x86_64.rpm
 
 grep -v '^#' ./rstudio.packages | xargs dnf -y install
+dnf -y install rstudio-2024.12.1-563-x86_64.rpm
