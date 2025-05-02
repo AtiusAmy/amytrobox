e5b93b9bee8d2c9d29e00200a794b5bc9b2498db
Atius Amy <torv.device027@passmail.net>
2025-05-03 04:44:18 +0700

    Things

--- scripts/worldpainterbox.sh
+++ scripts/worldpainterbox.sh
@@ -11,8 +11,3 @@ wget -P /tmp/ https://www.worldpainter.net/files/worldpainter_2.24.0.deb
 
 apt install /tmp/worldpainter_2.24.0.deb -y
 rm /tmp/worldpainter_2.24.0.deb
-curl -s https://julianfairfax.codeberg.page/package-repo/pub.gpg | gpg --dearmor | dd of=/usr/share/keyrings/julians-package-repo.gpg
-
-echo 'deb [ signed-by=/usr/share/keyrings/julians-package-repo.gpg ] https://julianfairfax.codeberg.page/package-repo/debs packages main' | tee /etc/apt/sources.list.d/julians-package-repo.list
-apt update -y && apt install adw-gtk3 -y
-
