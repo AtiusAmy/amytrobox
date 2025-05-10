1a3c5d2dfddf5126a23161af368a18f20d0ae226
Atius Amy <torv.device027@passmail.net>
2025-05-10 09:10:16 +0700

    yes

--- scripts/blenderbox.sh
+++ scripts/blenderbox.sh
@@ -4,5 +4,5 @@
 ./distrobox-shims.sh
 
 # Update the container and install packages
-pacman -Syu
-grep -v '^#' ./blenderbox.packages | xargs pacman -S
+pacman -Syu -y
+grep -v '^#' ./blenderbox.packages | xargs pacman -S -y
