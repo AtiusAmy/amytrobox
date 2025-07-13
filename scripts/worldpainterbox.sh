17dff04be238a3356e0665a472f24f6e979aa802
AtiusAmy <torv.device027@passmail.net>
2025-07-14 06:06:49 +0700

    Oops

--- scripts/worldpainterbox.sh
+++ scripts/worldpainterbox.sh
@@ -9,5 +9,5 @@ apt update -y && apt upgrade -y
 apt install wget tar -y
 wget -P /tmp/ https://www.worldpainter.net/files/worldpainter_2.25.1.deb
 
-apt install /tmp/worldpainter_2.24.1.deb -y
-rm /tmp/worldpainter_2.24.1.deb
+apt install /tmp/worldpainter_2.25.1.deb -y
+rm /tmp/worldpainter_2.25.1.deb
