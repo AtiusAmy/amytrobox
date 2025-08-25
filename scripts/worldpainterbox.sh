610045283fe295063e94e6036b83ad10f3a359ef
AtiusAmy <torv.device027@passmail.net>
2025-08-26 06:05:56 +0700

    Update WorldPainter

--- scripts/worldpainterbox.sh
+++ scripts/worldpainterbox.sh
@@ -7,7 +7,7 @@
 
 apt update -y && apt upgrade -y
 apt install wget tar -y
-wget -P /tmp/ https://www.worldpainter.net/files/worldpainter_2.25.1.deb
+wget -P /tmp/ https://www.worldpainter.net/files/worldpainter_2.26.0.deb
 
-apt install /tmp/worldpainter_2.25.1.deb -y
-rm /tmp/worldpainter_2.25.1.deb
+apt install /tmp/worldpainter_2.26.0.deb -y
+rm /tmp/worldpainter_2.26.0.deb
