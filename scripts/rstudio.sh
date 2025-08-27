27364989c3efdde750190b2e102a466cb7a3fab4
AtiusAmy <torv.device027@passmail.net>
2025-08-28 05:15:19 +0700

    DO it again

--- scripts/rstudio.sh
+++ scripts/rstudio.sh
@@ -6,13 +6,12 @@
 # Update the container and install packages
 
 dnf -y update
-dnf -y install wget tar epel-release
+dnf -y install wget epel-release
 dnf config-manager --set-enabled crb
 wget https://download1.rstudio.org/electron/rhel9/x86_64/rstudio-2025.05.1-513-x86_64.rpm
 
 grep -v '^#' ./rstudio.packages | xargs dnf -y install
 dnf -y install rstudio-2025.05.1-513-x86_64.rpm
 rm rstudio-2025.05.1-513-x86_64.rpm
-wget -P /usr/share/themes https://github.com/lassekongo83/adw-gtk3/releases/download/v6.1/adw-gtk3v6.1.tar.xz
-tar -xvf /usr/share/themes/adw-gtk3v6.1.tar.xz
+dnf -y install https://rpmfind.net/linux/fedora/linux/updates/41/Everything/x86_64/Packages/a/adw-gtk3-theme-5.6-1.fc41.noarch.rpm
 
