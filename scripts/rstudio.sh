ea0669e1e47f6998b3ff05ed8c884d7715a0cc36
AtiusAmy <torv.device027@passmail.net>
2025-08-28 04:16:54 +0700

    update rstudio

--- scripts/rstudio.sh
+++ scripts/rstudio.sh
@@ -7,11 +7,11 @@
 
 dnf -y update
 dnf -y install wget tar
-wget https://download1.rstudio.org/electron/rhel9/x86_64/rstudio-2024.12.1-563-x86_64.rpm
+wget https://download1.rstudio.org/electron/rhel9/x86_64/rstudio-2025.05.1-513-x86_64.rpm
 
 grep -v '^#' ./rstudio.packages | xargs dnf -y install
-dnf -y install rstudio-2024.12.1-563-x86_64.rpm
-rm rstudio-2024.12.1-563-x86_64.rpm
+dnf -y install rstudio-2025.05.1-513-x86_64.rpm
+rm rstudio-2025.05.1-513-x86_64.rpm
 wget -P /usr/share/themes https://github.com/lassekongo83/adw-gtk3/releases/download/v6.1/adw-gtk3v6.1.tar.xz
 tar -xvf /usr/share/themes/adw-gtk3v6.1.tar.xz
 
