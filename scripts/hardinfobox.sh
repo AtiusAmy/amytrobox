a4f8616aeeaf49a445940f8d9feac1c63bfb2d9f
AtiusAmy <torv.device027@passmail.net>
2026-01-09 07:38:37 +0700

    add testing

--- scripts/hardinfobox.sh
+++ scripts/hardinfobox.sh
@@ -4,5 +4,6 @@
 ./distrobox-shims.sh
 
 # Update the container and install packages
+echo "@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> <filename>
 apk update && apk upgrade
 grep -v '^#' ./hardinfobox.packages | xargs apk add
